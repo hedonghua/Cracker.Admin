927378858e917a8842456fa9fa244afbfeef1d3b
hedonghua <crackerwork@163.com>
2024-12-29 15:35:42 +0800

    服务监控部分

---
+++ backend/src/Cracker.Admin.Application/Monitor/ServerMonitorService.cs
@@ -0,0 +1,30 @@
+﻿using Cracker.Admin.Monitor.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Cracker.Admin.Monitor
+{
+    public class ServerMonitorService : ApplicationService, IServerMonitorService
+    {
+        public async Task<ServerMonitorInfoDto> GetServerMonitorInfoAsync()
+        {
+            var model = new ServerMonitorInfoDto()
+            {
+                Dotnet = new Dictionary<string, string>()
+                {
+                    ["version"] = ".net" + Environment.Version.ToString()
+                },
+                Server = new Dictionary<string, string>()
+                {
+                    ["machineName"] = Environment.MachineName,
+                    ["osVersion"] = Environment.OSVersion.ToString(),
+                    ["systemDirectory"] = Environment.SystemDirectory
+                }
+            };
+
+            return await Task.FromResult(model);
+        }
+    }
+}
