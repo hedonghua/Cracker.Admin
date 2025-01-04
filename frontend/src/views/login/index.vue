dbc2a0f56da18036c80718468b3b48078410a084
hedonghua <crackerwork@outlook.com>
2025-01-04 22:52:45 +0800

    更改登录ui

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -1,44 +1,37 @@
 <template>
-  <div class="login-container">
-    <el-row class="h-full">
-      <el-col :span="12" class="z-10 bg-sky-500">
-        <div class="flex flex-col justify-center w-full h-full">
-          <div class="content">
-            <p>{{ APP_TITLE }}，纯手写后台管理框架，不依赖任何Admin框架，使用Vite构建，UI框架ElementPlus，封装了一些常用组件，例如：图片组件、智能表格组件、tag标签组件等等。</p>
-          </div>
-          <img :src="LoginSvg" class="login-img" />
-        </div>
-      </el-col>
-      <el-col :span="12">
-        <div class="flex flex-col items-center justify-center min-h-full">
-          <div class="login-form-container">
-            <h2>{{ APP_TITLE }}后台登录</h2>
-            <el-form :model="form" :rules="rules" ref="formRef">
-              <el-form-item prop="username">
-                <el-input v-model="form.username" placeholder="请输入登录账号">
-                  <template #prepend>
-                    <el-icon>
-                      <User />
-                    </el-icon>
-                  </template>
-                </el-input>
-              </el-form-item>
-              <el-form-item prop="password">
-                <el-input type="password" show-password v-model="form.password" placeholder="请输入登录密码">
-                  <template #prepend><el-icon>
-                      <Lock />
-                    </el-icon></template>
-                </el-input>
-              </el-form-item>
-              <el-form-item>
-                <el-button type="primary" class="w-full" @click="login">登录</el-button>
-              </el-form-item>
-            </el-form>
-            <p class="text-gray-600">发现问题，请联系作者：crackerwork@outlook.com</p>
-          </div>
-        </div>
-      </el-col>
-    </el-row>
+  <div class="login-container"
+    :style="{ backgroundImage: `url(${LoginBg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }">
+    <div class="flex flex-col items-center justify-center h-full">
+      <div class="login-form-container">
+        <p class="login-title">{{ APP_TITLE }}后台登录</p>
+        <el-form :model="form" :rules="rules" ref="formRef" size="large">
+          <el-form-item prop="username">
+            <el-input v-model="form.username" placeholder="请输入登录账号">
+              <template #prepend>
+                <el-icon>
+                  <User />
+                </el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" show-password v-model="form.password" placeholder="请输入登录密码">
+              <template #prepend><el-icon>
+                  <Lock />
+                </el-icon></template>
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="w-full mt-2" @click="login">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+
+    <!-- 底部声明 -->
+    <p class="text-slate-300 text-sm absolute left-1/2 transform -translate-x-1/2 bottom-5">
+      发现问题，请联系作者：crackerwork@outlook.com
+    </p>
   </div>
 </template>
 
@@ -48,7 +41,7 @@ import { reactive, ref } from "vue";
 import { LoginForm, userLogin } from "@/api/login";
 import { useUserStore, UserAuthInfo } from "@/store/userStore"
 import { useRouter } from "vue-router";
-import LoginSvg from '@/assets/img/login.svg'
+import LoginBg from "@/assets/img/bg.jpg";
 import './index.styl'
 
 const APP_TITLE = import.meta.env.VITE_APP_TITLE;
