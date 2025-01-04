4bb29a6c56971ad5064cb95fdbe21e4e11f6bd5b
hedonghua <crackerwork@163.com>
2025-01-04 17:27:46 +0800

    修改端口

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig(({ command, mode }) => {
     server: {
       proxy: {
         "/api": {
-          target: "http://localhost:44383",
+          target: "http://localhost:5000",
           changeOrigin: true
         }
       },
